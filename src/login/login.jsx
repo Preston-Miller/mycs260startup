024e0a7211dae9838e60452ef8d1748f8a9ff299
Preston-Miller <millerpresto@gmail.com>
2024-12-10 01:35:35 -0700

    slight changes

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import '../app.css';
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main>
+        <h1>Welcome</h1>
+        <h3>The Community For Those Expecting</h3>
+        <form method="get" action="talk.html">
+            <div>
+                <input type="text" placeholder="email"/>
+            </div>
+            <div>
+                <input type="text" placeholder="password"/>
+            </div>
+            <div className="login_buttons">
+            <button type="submit">Login</button>
+            <button type="submit">Create Account</button>
+            </div>
+        </form>
     </main>
   );
 }
