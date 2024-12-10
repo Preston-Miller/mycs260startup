47cb9d1530f1fbab47126aa884f67a29bd3f520d
Preston-Miller <millerpresto@gmail.com>
2024-12-10 00:30:50 -0700

    added stubs

---
+++ src/talk/talk.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function talk() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>talk displayed here</div>
+    </main>
+  );
+}
---
+++ src/updates/updates.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function updates() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>updates displayed here</div>
+    </main>
+  );
+}
