c3582b61e6d7185ad74a9f9184babbb8d1675aac
Preston-Miller <millerpresto@gmail.com>
2024-12-11 13:34:26 -0700

    added on a few things from the instructions

--- service/index.js
+++ service/index.js
@@ -12,6 +12,9 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
